b45ebef5a6272cbdf4e880e54bbb9bab07cf1c35
Jo Emil Holen <jo.emil.holen@fremtind.no>
2020-03-29 22:34:13 +0200

    add weather and btc services

--- src/7seg-yolo.ino
+++ src/7seg-yolo.ino
@@ -1,6 +1,9 @@
 #include <SevSeg.h>
 #include <string.h>
 
+#define SPINNER_LENGTH 12
+#define SCREEN_RESET 30000
+
 SevSeg sevseg;
 
 byte digit1 = D5;
@@ -21,12 +24,12 @@ byte numDigits = 4;
 byte digitPins[] = {digit1, digit2, digit3, digit4};
 byte segmentPins[] = {a, b, c, d, e, f, g, dot};
 
-String displayCharacters;
-
 enum DisplayState
 {
   IDLE,
-  LOADING
+  LOADING,
+  YR,
+  BTC
 };
 
 enum BlinkState
@@ -43,6 +46,10 @@ String enumToString(DisplayState type)
     return "idle";
   case LOADING:
     return "loading";
+  case YR:
+    return "yr";
+  case BTC:
+    return "btc";
   default:
     return "invalid";
   }
@@ -52,18 +59,24 @@ DisplayState currentState = IDLE;
 BlinkState currentBlinkState = NOT_BLINKING;
 
 int setState(String state);
+void readWeather(const char *event, const char *data);
 
 unsigned long timer = millis();
+char *displayData = "";
 
-#define SPINNER_LENGTH 12
 byte spinnerSegments[SPINNER_LENGTH] = {0b00000001, 0b00000001, 0b00000001, 0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00001000, 0b00001000, 0b00001000, 0b00010000, 0b00100000};
 int spinnerDecimal[SPINNER_LENGTH] = {0, 1, 2, 3, 3, 3, 3, 2, 1, 0, 0, 0};
 unsigned int spinnerCurrentSegmentSelector = spinnerDecimal[0];
 
 void setup()
 {
-  Particle.function("set_state", setState);
   sevseg.begin(COMMON_ANODE, numDigits, digitPins, segmentPins);
+
+  Particle.function("set_state", setState);
+  Particle.function("trigger_function", triggerFunc);
+
+  Particle.subscribe("hook-response/MET_WeatherApi_Eidsvoll", readWeather, MY_DEVICES);
+  Particle.subscribe("hook-response/btc_eur", readBTCEur, MY_DEVICES);
 }
 
 void loop()
@@ -73,6 +86,7 @@ void loop()
     // TODO Support double blinking from time to time
     // TODO Adjust blink frequency
   case IDLE:
+  {
     switch (currentBlinkState)
     {
     case NOT_BLINKING:
@@ -94,13 +108,15 @@ void loop()
       break;
     }
     break;
+  }
 
   case LOADING:
+  {
     byte segments[4] = {0b00000000};
     segments[spinnerDecimal[spinnerCurrentSegmentSelector]] = spinnerSegments[spinnerCurrentSegmentSelector];
     sevseg.setSegments(segments);
 
-    if (millis() > (timer + 100))
+    if (millis() > (timer + 50))
     {
       spinnerCurrentSegmentSelector++;
       if (spinnerCurrentSegmentSelector >= SPINNER_LENGTH)
@@ -112,12 +128,38 @@ void loop()
     break;
   }
 
+  case YR:
+  {
+    sevseg.setChars(displayData);
+
+    if (millis() > (timer + SCREEN_RESET))
+    {
+      currentState = IDLE;
+    }
+    break;
+  }
+
+  case BTC:
+    if (millis() < (timer + 3000))
+    {
+      sevseg.setChars("BTC");
+    }
+    else if (millis() > (timer + SCREEN_RESET))
+    {
+      currentState = IDLE;
+    }
+    else
+    {
+      sevseg.setChars(displayData);
+    }
+  }
+
   sevseg.refreshDisplay();
 }
 
 int setState(String state)
 {
-  if (state.equals(enumToString(IDLE)))
+  if (state.equals(enumToString(IDLE)) || state.equals(""))
   {
     currentState = IDLE;
     return 0;
@@ -129,4 +171,47 @@ int setState(String state)
   }
 
   return -1;
+}
+
+int triggerFunc(String data)
+{
+  if (data.equals("yr"))
+  {
+    getWeather();
+    return 0;
+  }
+  else if (data.equals("btc"))
+  {
+    getBtc();
+    return 0;
+  }
+
+  return -1;
+}
+
+void getWeather()
+{
+  Particle.publish("MET_WeatherApi_Eidsvoll", PRIVATE);
+  currentState = LOADING;
+}
+
+void readWeather(const char *event, const char *data)
+{
+  currentState = YR;
+  displayData = strdup(data);
+  strcat(displayData, "*");
+  timer = millis();
+}
+
+void getBtc()
+{
+  Particle.publish("btc_eur", PRIVATE);
+  currentState = LOADING;
+}
+
+void readBTCEur(const char *event, const char *data)
+{
+  currentState = BTC;
+  displayData = strdup(data);
+  timer = millis();
 }
